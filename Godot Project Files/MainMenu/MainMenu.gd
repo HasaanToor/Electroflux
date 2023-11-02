a4ef26cbd11cca9eae7810dd3a04f39b5d4cb98b
Hasaan Toor <hasaan.m.toor@gmail.com>
2023-11-02 05:10:22 -0600

    Main menu implemented

---
+++ Godot Project Files/MainMenu/MainMenu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
+
+func _on_how_to_play_pressed():
+	get_tree().change_scene_to_file("res://MainMenu/HowToPlayMenu.tscn")
+
+func _on_exit_pressed():
+	get_tree().quit()
